017b934ac657ed0c91c87d2a5894bb4dd37b54b0
nathanW-QUT <110039213+nathanW-QUT@users.noreply.github.com>
2025-09-30 19:09:55 +1000

    "new fix attempt, just removing the argument in the constructor. Was: dao = new DistractionDAO(connection)"

--- src/test/java/DistractionDAOTest.java
+++ src/test/java/DistractionDAOTest.java
@@ -10,14 +10,11 @@ public class DistractionDAOTest {
     private DistractionDAO dao;
 
 
-    // This errors in the build for maven in github pages
-    // the issue is that new DistractionDAO(connection) expects a constuctor but has none.
-    // issue shows up after merging MurphyBed1's New-MainDistraction
-//    @BeforeEach
-//    public void setUp() throws SQLException {
-//        Connection connection = DriverManager.getConnection("jdbc:sqlite::memory:");
-//        dao = ;
-//    }
+    @BeforeEach
+    public void setUp() throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:sqlite::memory:");
+        dao = new DistractionDAO();
+    }
 
     @Test
     public void testAddDistraction() {
