5e103a9258f4217a044a0cd045f55c52a351e9ec
lisamurray <lisamurray084@gmail.com>
2025-10-17 23:10:08 +1000

    login incorrect shake - very slow

--- src/main/java/group13/demo1/controller/Login.java
+++ src/main/java/group13/demo1/controller/Login.java
@@ -3,14 +3,23 @@ package group13.demo1.controller;
 import group13.demo1.HelloApplication;
 import group13.demo1.model.UserDao;
 import group13.demo1.model.UserSession;
+
+import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.Node;
+
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
@@ -28,6 +37,19 @@ public class Login {
     private Label statusLabel;
 
     private final UserDao userDao = new UserDao();
+
+    @FXML
+    private VBox loginBox;
+
+    @FXML
+    public void initialize()
+    {
+        FadeTransition fadeIn = new FadeTransition(Duration.seconds(1.2), loginBox);
+        fadeIn.setFromValue(0);
+        fadeIn.setToValue(1);
+        fadeIn.play();
+    }
+
     @FXML
     private void onClickLogOut() throws IOException {
 
@@ -40,7 +62,6 @@ public class Login {
         String stylesheet = HelloApplication.class.getResource("stylesheet.css").toExternalForm();
         scene.getStylesheets().add(stylesheet);
     }
-
     @FXML
     private void handleLogin() throws IOException{
         String username = usernameField.getText();
@@ -57,9 +78,23 @@ public class Login {
             String stylesheet = HelloApplication.class.getResource("stylesheet.css").toExternalForm();
             scene.getStylesheets().add(stylesheet);
         } else {
+            statusLabel.setStyle("-fx-text-fill: red");
             statusLabel.setText("Invalid username or password.");
+            playShakeAnimation(usernameField);
+            playShakeAnimation(passwordField);
         }
     }
+
+    private void playShakeAnimation(Node node)
+    {
+        TranslateTransition shake = new TranslateTransition(Duration.seconds(0.8), node);
+        shake.setFromX(0);
+        shake.setToX(10);
+        shake.setCycleCount(6);
+        shake.setAutoReverse(true);
+        shake.play();
+    }
+
     @FXML
     private void onClickEditPassword() throws IOException {
         Stage stage = (Stage) nextButton.getScene().getWindow();
@@ -78,5 +113,4 @@ public class Login {
         String stylesheet = HelloApplication.class.getResource("stylesheet.css").toExternalForm();
         scene.getStylesheets().add(stylesheet);
     }
-
 }
